b4268e892d73c2a87d8aa45f48fdbb70b083354d
Fahad Ali Khan <fkhan195@myseneca.ca>
2024-09-20 02:48:08 -0400

    added common.hpp
    common includes are in this file

---
+++ cli_app/common.hpp
@@ -0,0 +1,21 @@
+// common.hpp - To include the most common files
+// 
+// 19-Sep-24  F.Khan         Created.
+
+#ifndef _COMMON_HPP_
+#define _COMMON_HPP_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <format>
+#include "conio.h"
+#include <map>
+#include <functional>
+#include <sstream>
+#include "dotenv.h"
+
+
+
+#endif // !_COMMON_HPP_
